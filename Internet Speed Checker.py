70dfbd7c55f2333af53eb10967f6bf8d35bd4aba
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-08-25 19:32:36 +0530

    Create Internet Speed Checker.py

---
+++ Internet Speed Checker.py
@@ -0,0 +1,26 @@
+from tkinter import*
+import speedtest
+def speedcheck(): 
+    sp=speedtest.Speedtest() 
+    sp.get_servers() 
+    down=str(round(sp.download()/(10**6),3))+" Mbps" 
+    up=str(round(sp.upload()/(10**6),3))+" Mbps" 
+    lab_down.config(text=down) 
+    lab_up.config(text=up) 
+sp=Tk() 
+sp.title("Internet Speed") 
+sp.geometry("10000x10000") 
+sp.config(bg="Red") 
+lab=Label(sp,text="Internet Speed Test",font=("Time New Roman",30,"bold"))
+lab.place(x=60,y=40,height=50,width=380)
+lab=Label(sp,text="Download Speed",font=("Time New Roman",30,"bold")) 
+lab.place(x=60,y=130,height=50,width=380) 
+lab_down=Label(sp,text="00",font=("Time New Roman",30,"bold")) 
+lab_down.place(x=60,y=200,height=50,width=380) 
+lab=Label(sp,text="Upload Speed",font=("Time New Roman",30,"bold")) 
+lab.place(x=60,y=290,height=50,width=380) 
+lab_up=Label(sp,text="00",font=("Time New Roman",30,"bold")) 
+lab_up.place(x=60,y=360,height=50,width=380) 
+button=Button(sp,text="CHECK SPEED",font=("Time New Roman",30,"bold"),relief=RAISED,bg="Yellow",command=speedcheck) 
+button.place(x=60,y=460,height=50,width=380) 
+sp.mainloop()
